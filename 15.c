8d6cd0751b1d06997cec2369e2d0de47a16d5b29
nessy254 <mukaminessy9@gmail.com>
2023-07-02 13:02:06 -0700

    w3 resource questions 10, 11, 15

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - calculate the factorial of a given number
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 5;
+	int i;
+	int factorial = 1;
+
+	for (i = 1; i <= n; i++)
+	{
+		factorial *= 1
+	}
+	printf("The factorial of %d is: %d\n", n, factorial);
+	return (0);
+}
